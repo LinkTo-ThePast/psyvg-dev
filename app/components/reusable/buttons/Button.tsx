293d6e57a566d8d12e6fbc83c8b4a3a10a160b24
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-25 19:08:12 -0600

    feat: adding global styles

--- app/components/reusable/buttons/Button.tsx
+++ app/components/reusable/buttons/Button.tsx
@@ -3,7 +3,7 @@ import IButton from "@/app/utils/interfaces/types";
 const variants = {
   primary: "bg-primary_pink",
   secondary: "bg-primary_white",
-  default: "bg-secondaru_white",
+  default: "bg-secondary_white",
 };
 
 const texts = {
@@ -12,8 +12,25 @@ const texts = {
   default: "text-primary_white scale",
 };
 
-const Button = ({ text, variant, type, disabled = false }: IButton) => {
-  return <Button></Button>;
+const Button = ({
+  text,
+  variant,
+  type,
+  disabled = false,
+  onClick,
+}: IButton) => {
+  return (
+    <button
+      className={`w-full p-4 pointer rounded-full ${variants[variant]} ${
+        texts[variant]
+      } ${disabled ? " hover cursor-not-allowed" : ""}`}
+      type={type ? type : "button"}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
