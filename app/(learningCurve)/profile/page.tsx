75984eb608e5d88722b255e3f4d75aa05ecf6873
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-04-14 14:24:00 -0600

    feat(auth): adding auth layout

---
+++ app/(learningCurve)/profile/page.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen " id="overlay-container">
+      <div
+      id="profile-container"
+      className="min-h-screen max-w-screen inset-2 bg-primary_green"
+    ></div>
+    </div>
+    
+  );
+};
+
+export default Home;
---
+++ app/components/layouts/auth/Auth.tsx
@@ -0,0 +1,13 @@
+const AuthLayout = () => {
+  return (
+    <section>
+      <header>
+        <h1>
+          ¡Ingresa para consultar tus detalles y revisar análisis pasados!
+        </h1>
+      </header>
+    </section>
+  );
+};
+
+export default AuthLayout;
