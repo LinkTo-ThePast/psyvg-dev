734c02f31ad4b7497bc5b786636b54fbbd8ce4d8
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-04-18 16:59:14 -0600

    feat(landing): adding main layout

--- app/components/landing/Landing.tsx
+++ app/components/landing/Landing.tsx
@@ -1,10 +1,12 @@
 import MainLayout from "../layouts/main-layout/MainLayout";
 import Header from "./Header";
+import MainSection from "./MainSeciton";
 
 const Landing = () => {
   return (
     <MainLayout>
       <Header />
+      <MainSection />
     </MainLayout>
   );
 };
---
+++ app/components/landing/MainSeciton.tsx
@@ -0,0 +1,9 @@
+const MainSection = () => {
+  return (
+    <section className="flex-1 container mx-auto px-4 md:px-6 flex flex-col items-center justify-center relative py-8 md:py-16 mt-4 md:mt-0 text-primary_white">
+      <h1>¡Conoce tu perfil como videojugador!</h1>
+    </section>
+  );
+};
+
+export default MainSection;
