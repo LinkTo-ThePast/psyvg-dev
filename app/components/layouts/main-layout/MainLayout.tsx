9771cc9958caaf2c21c22e4dbfd0aa401341d8c0
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-04-19 15:16:16 -0600

    feat(landing): adding main card transition

--- app/components/layouts/main-layout/MainLayout.tsx
+++ app/components/layouts/main-layout/MainLayout.tsx
@@ -6,16 +6,22 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const [initialLayout, setInitialLayout] = useState<boolean>(true);
   // trigger effect
   useEffect(() => {
+    // initial point: green card takes all the viewport; final state: green card starts to shinrk!
     setInitialLayout(false);
   }, []);
 
   return (
-    <main id="main-container" className={`min-h-screen bg-primary_white`}>
+    <main
+      id="main-container"
+      className={`min-h-screen bg-primary_white relative overflow-hidden`}
+    >
       <div
         id="main-card"
-        className={`bg-primary_green w-full min-h-screen ${
-          initialLayout ? "rounded-none" : "rounded-3xl p-6"
-        } transition-all duration-700 ease-in-out`}
+        className={`bg-primary_green transition-all duration-700 ease-in-out ${
+          initialLayout
+            ? "rounded-none p-0 w-full h-screen"
+            : "rounded-3xl p-6 h-[95vh] w-[95vw]"
+        } absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
       >
         {children}
       </div>
