80b1d21ae208903f40d5665b72531c1a1ba2dbc8
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-04-20 02:57:47 -0600

    feat(landing): adding questions

---
+++ app/utils/survey/questions/survey.ts
@@ -0,0 +1,14 @@
+const survey = {
+  // First question!
+  "¿Cuál es tu género?": [
+    "Femenino",
+    "Masculino",
+    "No binario",
+    "Prefiero describirme",
+  ],
+  // Second question!
+  "Cuántos años tienes?": [],
+  // Third question!
+  "En una semana normal o tranquila, ¿cuántos días juegas videojuegos al menos 30 minutos?":
+    ["0-1", "2-3", "4-5", "6-7"],
+};
